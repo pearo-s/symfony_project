eff52e8194e85d4e83dcc07d3530e02b86f8afd5
Sultan Bekbosun <sultanbb28@gmail.com>
2025-03-04 16:12:07 +0600

    fixed HomeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,16 +11,6 @@ final class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(): Response
     {
-        $url = 'https://youtube.com';
-
-        $curl = curl_init();
-
-        curl_setopt($curl, CURLOPT_URL, $url);
-
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-
-        $res = curl_exec($url);
-
-        dd($res);
+        return $this->redirectToRoute('main_index_post');
     }
 }
